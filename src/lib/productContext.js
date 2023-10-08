d396c20d437daf2df68fbcf74d1ef3e6af0985df
Akshay Dubey <akshaydubey@osiansoftware.com>
2023-10-08 18:50:41 +0530

    caching testing

--- src/lib/productContext.js
+++ src/lib/productContext.js
@@ -9,7 +9,8 @@ export const ProductContextProvider = ({ children }) => {
     // Fetch data from the API and set it to the state
     async function fetchData() {
       try {
-        const response = await fetch("/api/airtable",{cache: 'no-store'});
+        const { signal } = new AbortController()
+        const response = await fetch("/api/airtable",{signal});
         const responseBody = await response.json()
         setApiData(responseBody.filterData);
       } catch (error) {
