2c3124469ef39802ea1597fb68267ac2607eb745
Akshay Dubey <akshaydubey@osiansoftware.com>
2023-10-15 11:39:44 +0530

    1. Auth form UI Desgin

--- src/components/auth-form/AuthForm.tsx
+++ src/components/auth-form/AuthForm.tsx
@@ -2,18 +2,22 @@ import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default   function AuthForm() {
+export default function AuthForm() {
   const supabase = createClientComponentClient<any>();
 
   return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
-      showLinks={false}
-      providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
-    />
+    <>
+      <div className="">
+        <Auth
+          supabaseClient={supabase}
+          view="magic_link"
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+          showLinks={false}
+          providers={[]}
+          redirectTo="http://localhost:3000/auth/callback"
+        />
+      </div>
+    </>
   );
 }
