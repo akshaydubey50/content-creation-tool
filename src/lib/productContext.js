92d025d0f00e23ebb6dd98c16361feae3c5d360f
Akshay Dubey <akshaydubey@osiansoftware.com>
2023-10-15 09:32:16 +0530

    1. Added swr package to api loaded & caching work efficiently

--- src/lib/productContext.js
+++ src/lib/productContext.js
@@ -1,25 +1,53 @@
+import Loader from "@/components/spinner-loader/Loader";
 import React, { createContext, useContext, useEffect, useState } from "react";
+import useSWR from 'swr'
 
 const ProductContext = createContext();
 
 export const ProductContextProvider = ({ children }) => {
   const [apiData, setApiData] = useState([]);
 
-  useEffect(() => {
-    // Fetch data from the API and set it to the state
-    async function fetchData() {
-      try {
-        const { signal } = new AbortController()
-        const response = await fetch("/api/airtable",{signal});
-        const responseBody = await response.json()
-        setApiData(responseBody.filterData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+
+  const fetcher = async () => {
+    const response = await fetch("/api/airtable");
+    const responseBody = await response.json();
+    return responseBody.filterData;
+  };
+  const {data,error} =   useSWR("/api/airtable",fetcher)
+
+    useEffect(() => {
+      if (data && !error) {
+        setApiData(data);
       }
+    }, [data, error]);
+  
+    if(error) {
+      return 'An error has occured'
+    }
+    if(!data) {
+      return (
+        <Loader />
+      )
     }
 
-    fetchData();
-  }, [setApiData]);
+
+
+
+  // useEffect(() => {
+  //   // Fetch data from the API and set it to the state
+  //   async function fetchData() {
+  //     try {
+  //       const { signal } = new AbortController()
+  //       const response = await fetch("/api/airtable",{signal});
+  //       const responseBody = await response.json()
+  //       setApiData(responseBody.filterData);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   }
+
+  //   fetchData();
+  // }, [setApiData]);
 
   return (
     <ProductContext.Provider value={{ apiData }}>
