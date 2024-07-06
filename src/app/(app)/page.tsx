89714ff1cd7c7743dbfafea0399378493d1aca58
Akshay Dubey <akshaydubey@osiansoftware.com>
2024-07-06 18:51:58 +0530

    pagination handle in ui

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/card/ProductList";
 import HeroSection from "@/components/herosection/HeroSection";
 import FilterSection from "@/components/filter/FilterSection";
+import Script from 'next/script'
 
 export const dynamic = 'force-dynamic'
 
@@ -9,8 +10,13 @@ export default function Home() {
   return (
     <div className="mb-8">
       <HeroSection />
+      {/* <div className="launchlist-widget" data-key-id="sFJR8e" data-height="180px"></div> */}
       <FilterSection />
       <ProductList />
+      {/* <Script
+        src="https://getlaunchlist.com/js/widget.js"
+        defer
+      /> */}
     </div>
   );
 }
