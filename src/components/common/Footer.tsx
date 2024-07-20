8f72f3d5adfbc9c2d231e2ada67b64645cb291b5
Akshay Dubey <akshaydubey@osiansoftware.com>
2024-07-20 20:16:10 +0530

    update

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-black self-center text-2xl font-semibold whitespace-nowrap">
-            Content Tool
+            Content Creation
           </span>
           {/* </a> */}
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium  sm:mb-0 text-black">
