70bca0000644822c33041ee98ef1dabc6f8980f7
Akshay Dubey <akshaydubey2060@gmail.com>
2024-09-29 16:27:08 +0530

    sitemap updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,4 @@
 import { MetadataRoute } from "next";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://www.contentcreation.fyi/tools",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    },
-    {
-      url: "https://www.contentcreation.fyi/contact",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.8,
-    },
-    {
-      url: "https://www.contentcreation.fyi/prompt",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
-    },
-    {
-      url: "https://www.contentcreation.fyi/resources",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.5,
-    },
-  ];
-}
-import { MetadataRoute } from "next";
 import { APPConf } from "@/conf/conf";
 import {
   AirtableModel,
