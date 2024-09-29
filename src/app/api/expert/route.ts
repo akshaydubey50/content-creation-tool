ec74efc9bf9884feccb3c19d2c940898491ebc9f
Ankit <ankit60.av@gmail.com>
2024-09-29 15:37:21 +0530

    fix build issue

---
+++ src/app/api/expert/route.ts
@@ -0,0 +1,54 @@
+export const dynamic = "force-dynamic";
+import { NextResponse } from "next/server";
+import { AirtableModel } from "@/models/airtable.model";
+import axios from "axios";
+import { AirtableConf } from "@/conf/conf";
+import connectDB from "@/db/dbConnect";
+
+export async function GET() {
+  await connectDB();
+  const headers = {
+    Authorization: `Bearer ${AirtableConf.BEARER_TOKEN}`,
+  };
+
+  try {
+    let airtableProductList: AirtableModel[] = [];
+    let offset = null;
+    do {
+      const response: any = await axios.get(
+        `${AirtableConf.BASE_URL}/${AirtableConf.BASE_ID}/${AirtableConf.TABLE_ID}`,
+        {
+          headers,
+          params: {
+            offset,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const records: AirtableModel[] = (await response.data[
+          "records"
+        ]) as AirtableModel[];
+        airtableProductList.push(...records);
+        offset = response.data.offset;
+      }
+    } while (offset);
+    const statusRecord = airtableProductList?.filter(
+      (item: AirtableModel) => item.fields?.Stage?.toLowerCase() == "done"
+    );
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: statusRecord,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("expert api failure ::: ", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
