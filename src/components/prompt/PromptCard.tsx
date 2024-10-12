b629de2a7f9d698f7548e1d4639e438de5e26da3
Ankit <ankit60.av@gmail.com>
2024-10-12 19:15:38 +0530

    fix prompt issue

--- src/components/prompt/PromptCard.tsx
+++ src/components/prompt/PromptCard.tsx
@@ -7,21 +7,19 @@ import {
   CardFooter,
 } from "../ui/card";
 import { PropmtResourceModel } from "@/models/airtable.model";
-import { Button } from "../ui/button";
-import { useRouter } from "next/navigation";
-import { FiArrowUpRight } from "react-icons/fi";
 import { Badge } from "../ui/badge";
 import LikeButton from "../ui/likebutton";
 import BookmarkButton from "../ui/bookmarkbutton";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import VisitWebsite from "../visit-website/VisitWebsite";
+import { toKebabCase } from "@/helper/helper";
 
 const PromptCard = ({
   promptResource,
 }: {
   promptResource: PropmtResourceModel;
 }) => {
-  const router = useRouter();
   const likedPromptList = useSelector(
     (state: RootState) => state.likes.likedList
   );
@@ -67,14 +65,6 @@ const PromptCard = ({
     }
   }, [bookmarkedPromptList, promptResource?.id]);
 
-  const handlePromptClick = (promptResource: PropmtResourceModel) => {
-    // navigate to prompt detail page
-    const title = promptResource?.fields?.Name?.toLowerCase()
-      .trim()
-      .replace(/\s/g, "-");
-    router.push(`/prompt/${title}`);
-  };
-
   if (!promptResource) {
     return null;
   }
@@ -89,19 +79,6 @@ const PromptCard = ({
               </Badge>
             ))}
           </div>
-          {/* <button
-            title="Bookmark"
-            type="button"
-            className="flex items-center gap-x-2"
-            onClick={handleLike}
-          >
-            {isLiked ? (
-              <AiFillHeart className="text-2xl text-DarkOrange" />
-            ) : (
-              <AiOutlineHeart className="text-2xl text-black" />
-            )}
-            <span>{Math.floor(Math.random() * 10) + 1}</span>
-          </button> */}
           <LikeButton
             key={promptResource.id}
             initialLikedState={isAlreadyLiked}
@@ -118,21 +95,12 @@ const PromptCard = ({
         </p>
       </CardContent>
       <CardFooter className="">
-        <div className="flex items-center justify-between gap-x-4">
-          <Button
-            onClick={() => handlePromptClick(promptResource)}
-            className="px-4 py-2 font-semibold text-white border rounded-md  bg-DarkOrange hover:bg-DarkOrange/90 hover:bg-white hover:text-DarkOrange border-DarkOrange"
-          >
-            View Prompt
-            <FiArrowUpRight className="text-2xl " />
-          </Button>
-          {/* <button title="Bookmark" onClick={handleBoomark}>
-            {isBookMarked ? (
-              <BsBookmarkFill className="text-2xl text-DarkOrange" />
-            ) : (
-              <BsBookmark className="text-2xl text-black" />
-            )}
-          </button> */}
+        <div className="flex justify-between items-center gap-x-4">
+          <VisitWebsite
+            btnText="View Prompt"
+            url={`/prompt/${promptResource.fields?.Name}`}
+            openInNewTab={false}
+          />
           <BookmarkButton
             key={promptResource.id}
             isInitialBookmarked={isAlreadyBookmarked}
--- src/components/prompt/prompt-library.tsx
+++ src/components/prompt/prompt-library.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useCallback, useEffect, useState } from "react";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { AppDispatch, RootState } from "@/redux/store";
@@ -14,61 +13,67 @@ import Pagination from "../pagination/Pagination";
 
 export default function PromptLibrary() {
   const [currentPage, setCurrentPage] = useState(1);
-
   const [isCategoryOpen, setIsCategoryOpen] = useState(false);
   const [promptList, setPromptList] = useState<PropmtResourceModel[]>([]);
+  const [filteredPrompts, setFilteredPrompts] = useState<PropmtResourceModel[]>(
+    []
+  );
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
   const dispatch: AppDispatch = useDispatch();
   const { isLoading, isError, promptResourceList } = useSelector(
     (state: RootState) => state.promptResources
   );
 
-  const itemsPerPage = 6;
+  const itemsPerPage = 10;
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const getPromptCategory = () => {
+  const getPromptCategory = useCallback(() => {
     const categoryList = new Set();
     if (promptList.length > 0) {
-      promptList.map((prompt) => {
-        if (!categoryList?.has(prompt.fields?.Category?.[0])) {
-          categoryList.add(prompt.fields?.Category?.[0]);
+      promptList.forEach((prompt) => {
+        if (prompt.fields?.Category?.[0]) {
+          categoryList.add(prompt.fields.Category[0]);
         }
       });
     }
+    return Array.from(categoryList) as string[];
+  }, [promptList]);
 
-    return Array.from(categoryList);
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+    filterPrompts(category);
+    setCurrentPage(1);
   };
-
+  const filterPrompts = (category: string) => {
+    const filtered = promptList.filter(
+      (prompt) => prompt.fields?.Category?.[0] === category
+    );
+    setFilteredPrompts(filtered);
+  };
 
   const updateCurrentProducts = useCallback(() => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const newCurrentProducts = promptList!.slice(
-      startIndex,
-      endIndex
-    );
-    return newCurrentProducts;
-  }, [currentPage, promptList]);
+    const currentList = selectedCategory ? filteredPrompts : promptList;
+    return currentList.slice(startIndex, endIndex);
+  }, [currentPage, promptList, filteredPrompts, selectedCategory]);
 
   useEffect(() => {
-    updateCurrentProducts();
-  }, [currentPage, promptList, updateCurrentProducts]);
-
-  useEffect(() => {
-    if (promptResourceList.length === 0) {
+    if (promptResourceList?.length === 0) {
       dispatch(fetchPromptResourceList());
     }
   }, [dispatch, promptResourceList]);
 
   useEffect(() => {
-    console.log("useEffect Triggered!!!");
-    if (!isLoading && promptResourceList.length > 0) {
+    if (!isLoading && promptResourceList?.length > 0) {
       setPromptList(promptResourceList);
+      setFilteredPrompts(promptResourceList);
     }
-  }, [isLoading, promptResourceList, isError, getPromptCategory]);
+  }, [isLoading, promptResourceList, isError]);
 
   if (isLoading) {
     return <Loader />;
@@ -76,57 +81,60 @@ export default function PromptLibrary() {
 
   return (
     <>
-    <div className="flex flex-col pt-10 bg-background">
-      {/* Mobile Category Toggle */}
-      <div className="block p-4 lg:hidden">
-        <Button
-          className="flex items-center justify-between w-full border-black bg-light-gray"
-          onClick={() => setIsCategoryOpen(!isCategoryOpen)}
-        >
-          Categories
-          {isCategoryOpen ? (
-            <ChevronUp className="w-4 h-4" />
-          ) : (
-            <ChevronDown className="w-4 h-4" />
-          )}
-        </Button>
-      </div>
+      <div className="flex flex-col  bg-background pt-10">
+        {/* Mobile Category Toggle */}
+        <div className="block lg:hidden p-4">
+          <Button
+            className="w-full flex justify-between items-center border-black bg-light-gray"
+            onClick={() => setIsCategoryOpen(!isCategoryOpen)}
+          >
+            Categories
+            {isCategoryOpen ? (
+              <ChevronUp className="h-4 w-4" />
+            ) : (
+              <ChevronDown className="h-4 w-4" />
+            )}
+          </Button>
+        </div>
 
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-12 lg:gap-6 ">  
-        {/* Left Sidebar - Categories */}
-        <aside
-          className={`col-span-1 lg:col-span-3 p-6 ${
-            isCategoryOpen ? "block" : "hidden"
-          } lg:block`}
-        >
-          <PromptCategory categoryList={getPromptCategory()} />
-        </aside>
+        <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-6 gap-4 ">
+          {/* Left Sidebar - Categories */}
+          <aside
+            className={`col-span-1 lg:col-span-3 p-6 ${
+              isCategoryOpen ? "block" : "hidden"
+            } lg:block`}
+          >
+            <PromptCategory
+              categoryList={getPromptCategory()}
+              onSelectCategory={handleCategorySelect}
+              selectedCategory={selectedCategory}
+            />
+          </aside>
 
-        {/* Main Content */}
-        {/* {isLoading && <Loader />} */}
-        {!isLoading && promptList.length > 0 && (
-          <main className="col-span-1 p-4 lg:col-span-9 md:p-6">
-            {/* <Input
+          {/* Main Content */}
+          {/* {isLoading && <Loader />} */}
+          {!isLoading && promptList.length > 0 && (
+            <main className="col-span-1 lg:col-span-9 p-4 md:p-6">
+              {/* <Input
               className="mb-6 ring-DarkOrange ring-opacity-50"
               placeholder="Looking for a prompt?"
             /> */}
-              <div className="grid grid-cols-1 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 md:gap-6"> 
+              <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-3 md:gap-6">
                 {updateCurrentProducts().map((prompt, index) => (
-                <PromptCard key={index} promptResource={prompt} />
-              ))}
-            </div>
-                <Pagination
-                  currentPage={currentPage}
-                  totalItems={promptList!.length}
-                  onPageChange={handlePageChange}
-                />
-          </main>
-        )}
+                  <PromptCard key={index} promptResource={prompt} />
+                ))}
+              </div>
+              <Pagination
+                currentPage={currentPage}
+                totalItems={
+                  selectedCategory ? filteredPrompts.length : promptList.length
+                }
+                onPageChange={handlePageChange}
+              />
+            </main>
+          )}
+        </div>
       </div>
-    </div>
-    
-      
     </>
-
   );
 }
