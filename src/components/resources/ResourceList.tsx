6854ca733c906205a582ac6564a53ce68df7857a
Akshay Dubey <akshaydubey2060@gmail.com>
2024-10-12 13:34:01 +0530

    save

--- src/components/resources/ResourceList.tsx
+++ src/components/resources/ResourceList.tsx
@@ -19,7 +19,6 @@ export default function ResourceList() {
     const [currentPage, setCurrentPage] = useState(1);
 
     const dispatch: AppDispatch = useDispatch();
-    const { isUserAuthenticated } = useSelector((store: RootState) => store.user);
     const { data: session } = useSession();
     const { resourceList, isLoading } = useSelector(
         (state: RootState) => state.resources
@@ -65,23 +64,23 @@ export default function ResourceList() {
 
     return (
         <>
-            <section className='grid grid-cols-1 md:grid-cols-12 gap-6 lg:gap-10 max-w-8xl mx-auto py-5 justify-items-center px-8  2xl:px-0'>
+            <section className='grid grid-cols-1 gap-6 px-8 py-5 mx-auto md:grid-cols-12 lg:gap-10 max-w-8xl justify-items-center 2xl:px-0'>
                 {resourceList?.length > 0 &&
                     (<>
-                    <div className="col-span-1  md:col-span-2 w-full ">
+                    <div className="w-full col-span-1 md:col-span-2 ">
                         <div className="block lg:hidden ">
                             <Button
-                                className="w-full flex justify-between items-center border-black bg-light-gray"
+                                className="flex items-center justify-between w-full border-black bg-light-gray"
                                 onClick={() => setIsCategoryOpen(!isCategoryOpen)}
                             >
                                 Categories
                                 {isCategoryOpen ? (
                                    <>
-                                        <ChevronUp className="h-4 w-4" />
+                                        <ChevronUp className="w-4 h-4" />
                                    </>
 
                                 ) : (
-                                    <ChevronDown className="h-4 w-4" />
+                                    <ChevronDown className="w-4 h-4" />
                                 )}
                             </Button> 
                             {isCategoryOpen && (<ResourceSidebar />)}
@@ -93,8 +92,7 @@ export default function ResourceList() {
                     </div>
                     <div className="col-span-1 md:col-span-10 ">
                             <main
-                            className="grid grid-cols-1 gap-y-6 md:grid-cols-2  md:gap-8 lg:grid-cols-3 
-                  lg:gap-10  w-fit  mx-auto   justify-items-center "
+                            className="grid grid-cols-1 mx-auto gap-y-6 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 w-fit justify-items-center "
                         >
                             {resourceList?.map((item: ResourceModel) => {
                                 if (!item || !item.id) return null; // Add this line
