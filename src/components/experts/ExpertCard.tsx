450b3b6241e7a0492d6a9828652900b51db995e0
Akshay Dubey <akshaydubey2060@gmail.com>
2025-01-18 19:40:57 +0530

    ui and sitemap

--- src/components/experts/ExpertCard.tsx
+++ src/components/experts/ExpertCard.tsx
@@ -6,7 +6,7 @@ import { CheckCircle, User } from 'lucide-react'
 
 export default function ExpertCard({ expert }: any) {
   console.log("expert", expert)
-  const { "First Name": firstName, "Last Name": lastName, ExpertType, ProfileImage, "Professional Bio": professionalBio, Verified, Country, Headline, Skills:skills } = expert;
+  const { "First Name": firstName, "Last Name": lastName, ExpertType, ProfileImage, "Professional Bio": professionalBio, Verified, Country, Headline, Skills: skills } = expert;
 
   const defaultImage = <User />
 
@@ -22,10 +22,10 @@ export default function ExpertCard({ expert }: any) {
             className="rounded-full"
           />
           <div className="flex-1 min-w-0 space-y-1">
-            <div className="flex justify-between items-start">
-              <div className="flex items-center space-x-2">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-4">
                 <p className="text-lg font-medium text-gray-900">
-                  {`${firstName} ${lastName}`} <span className="text-gray-500 uppercase">{Country.slice(0,2)}</span>
+                  {`${firstName} ${lastName}`} <span className="text-gray-500 ml-4 uppercase">{Country.slice(0, 2)}</span>
                 </p>
                 {Verified && (
                   <Badge variant="secondary" className="text-xs bg-orange-100 text-orange-700 border-orange-200">
@@ -41,7 +41,7 @@ export default function ExpertCard({ expert }: any) {
             <p className="text-base text-gray-500">
               {Headline}
             </p>
-            <div className="flex flex-wrap gap-2">
+            <div className="flex flex-wrap gap-2 pt-2">
               {skills?.slice(0, 5)?.map((skill: any, skillIndex: number) => (
                 <Badge key={skillIndex} variant="secondary" className="text-xs">
                   {skill}
@@ -53,7 +53,7 @@ export default function ExpertCard({ expert }: any) {
                 </Badge>
               )}
             </div>
-            <p className="text-sm text-gray-600 line-clamp-2">
+            <p className="text-sm text-gray-600 line-clamp-2 pt-2">
               {professionalBio}
             </p>
           </div>
